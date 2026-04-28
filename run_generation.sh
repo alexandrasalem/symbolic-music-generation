038d45e10349899e8503b2548b2f19f0e625da7f
Alex Salem <alexandrasalem8@gmail.com>
2026-04-28 00:20:56 -0400

    updating bash scripts

--- run_generation.sh
+++ run_generation.sh
@@ -1,4 +1,105 @@
 #!/bin/bash
 
-echo "pulling down files"
+echo "making folders"
+mkdir checkpoints
+mkdir checkpoints/chord2bass_train_checkpoints
+mkdir checkpoints/chord2melody_train_checkpoints
+mkdir checkpoints/chord2bass_theme_train_checkpoints
+mkdir checkpoints/chord2melody_theme_train_checkpoints
 
+mkdir checkpoints/nothingprior2bass_train_checkpoints
+mkdir checkpoints/nothingprior2melody_train_checkpoints
+mkdir checkpoints/nothingprior2bass_theme_train_checkpoints
+mkdir checkpoints/nothingprior2melody_theme_train_checkpoints
+
+mkdir checkpoints/chord2sequentialbass_first_train_checkpoints
+mkdir checkpoints/chord2sequentialmelody_first_train_checkpoints
+mkdir checkpoints/chord2sequentialbass_first_theme_train_checkpoints
+mkdir checkpoints/chord2sequentialmelody_first_theme_train_checkpoints
+
+mkdir checkpoints/chord2symmetricdecoder_train_checkpoints
+mkdir checkpoints/chord2symmetricdecoder_theme_train_checkpoints
+
+mkdir checkpoints/chord2jointdecoder_train_checkpoints
+mkdir checkpoints/chord2jointdecoder_theme_train_checkpoints
+
+mkdir logs
+mkdir samples
+
+
+echo "pulling down log files"
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2bass_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2melody_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2bass_theme_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2melody_theme_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2bass_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2melody_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2bass_theme_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2melody_theme_train_log.log ./logs
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialbass_first_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialmelody_first_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialbass_first_theme_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialmelody_first_theme_train_log.log ./logs
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2symmetricdecoder_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2symmetricdecoder_theme_train_log.log ./logs
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2jointdecoder_train_log.log ./logs
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2jointdecoder_theme_train_log.log ./logs
+
+echo "pulling down checkpoints"
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2bass_train_checkpoints/chord2bass_epoch_100.pt ./checkpoints/chord2bass_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2melody_train_checkpoints/chord2melody_epoch_100.pt ./checkpoints/chord2melody_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2bass_theme_train_checkpoints/chord2bass_theme_epoch_100.pt ./checkpoints/chord2bass_theme_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2melody_theme_train_checkpoints/chord2melody_theme_epoch_100.pt ./checkpoints/chord2melody_theme_train_checkpoints
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2bass_train_checkpoints/nothingprior2bass_epoch_100.pt ./checkpoints/nothingprior2bass_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2melody_train_checkpoints/nothingprior2melody_epoch_100.pt ./checkpoints/nothingprior2melody_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2bass_theme_train_checkpoints/nothingprior2bass_theme_epoch_100.pt ./checkpoints/nothingprior2bass_theme_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/nothingprior2melody_theme_train_checkpoints/nothingprior2melody_theme_epoch_100.pt ./checkpoints/nothingprior2melody_theme_train_checkpoints
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialbass_first_train_checkpoints/chord2sequentialbass_first_epoch_100.pt ./checkpoints/chord2sequentialbass_first_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialmelody_first_train_checkpoints/chord2sequentialmelody_first_epoch_100.pt ./checkpoints/chord2sequentialmelody_first_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialbass_first_theme_train_checkpoints/chord2sequentialbass_first_theme_epoch_100.pt ./checkpoints/chord2sequentialbass_first_theme_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2sequentialmelody_first_theme_train_checkpoints/chord2sequentialmelody_first_theme_epoch_100.pt ./checkpoints/chord2sequentialmelody_first_theme_train_checkpoints
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2symmetricdecoder_train_checkpoints/chord2symmetricdecoder_epoch_100.pt ./checkpoints/chord2symmetricdecoder_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2symmetricdecoder_theme_train_checkpoints/chord2symmetricdecoder_theme_epoch_100.pt ./checkpoints/chord2symmetricdecoder_theme_train_checkpoints
+
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2jointdecoder_train_checkpoints/chord2jointdecoder_epoch_100.pt ./checkpoints/chord2jointdecoder_train_checkpoints
+rsync -avP alexandra@146.245.250.218:/scratch/alexandra/symbolic-music-generation/chord2jointdecoder_theme_train_checkpoints/chord2jointdecoder_theme_epoch_100.pt ./checkpoints/chord2jointdecoder_theme_train_checkpoints
+
+echo "generating samples from checkpoints"
+python generate_no_chords.py --piece_or_theme=piece --bass_or_melody=bass --epoch=100
+python generate_no_chords.py --piece_or_theme=theme --bass_or_melody=bass --epoch=100
+python generate_no_chords.py --piece_or_theme=piece --bass_or_melody=melody --epoch=100
+python generate_no_chords.py --piece_or_theme=theme --bass_or_melody=melody --epoch=100
+
+python generate.py --piece_or_theme=piece --bass_or_melody=bass --epoch=100
+python generate.py --piece_or_theme=theme --bass_or_melody=bass --epoch=100
+python generate.py --piece_or_theme=piece --bass_or_melody=melody --epoch=100
+python generate.py --piece_or_theme=theme --bass_or_melody=melody --epoch=100
+
+python generate_sequential.py --piece_or_theme=piece --bass_or_melody=bass --epoch=100
+python generate_sequential.py --piece_or_theme=theme --bass_or_melody=bass --epoch=100
+python generate_sequential.py --piece_or_theme=piece --bass_or_melody=melody --epoch=100
+python generate_sequential.py --piece_or_theme=theme --bass_or_melody=melody --epoch=100
+
+python generate_symmetric.py --piece_or_theme=piece --epoch=100
+python generate_symmetric.py --piece_or_theme=theme --epoch=100
+
+python generate_jointdecoder.py --piece_or_theme=piece --epoch=100
+python generate_jointdecoder.py --piece_or_theme=theme --epoch=100
+
+echo "combining samples"
+
+python combine_midi.py --bass_location=samples/chord2symmetricdecoder_theme_samples/generated_midis_100/bass
+--melody_location=samples/chord2symmetricdecoder_theme_samples/generated_midis_100/melody
+--combined_location=samples/chord2symmetricdecoder_theme_samples/generated_midis_100/combined
+--test_csv_location=test_joint_themes_held_out.csv
+
+python combine_midi.py --bass_location=samples/chord2sequentialmelody_first_samples/generated_midis_100/bass
+--melody_location=samples/chord2sequentialmelody_first_samples/generated_midis_100/melody
+--combined_location=samples/chord2sequentialmelody_first_samples/generated_midis_100/combined
+--test_csv_location=test_joint.csv
--- run_training.sh
+++ run_training.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#echo "running nothingprior2bass and nothingprior2melody AKA no chord (bass and melody, separately)"
-#python train_model_no_chords.py --bass_or_melody=bass --piece_or_theme=piece
-#python train_model_no_chords.py --bass_or_melody=bass --piece_or_theme=theme
-#python train_model_no_chords.py --bass_or_melody=melody --piece_or_theme=piece
-#python train_model_no_chords.py --bass_or_melody=melody --piece_or_theme=theme
+echo "running nothingprior2bass and nothingprior2melody AKA no chord (bass and melody, separately)"
+python train_model_no_chords.py --bass_or_melody=bass --piece_or_theme=piece
+python train_model_no_chords.py --bass_or_melody=bass --piece_or_theme=theme
+python train_model_no_chords.py --bass_or_melody=melody --piece_or_theme=piece
+python train_model_no_chords.py --bass_or_melody=melody --piece_or_theme=theme
 
 echo "running chord2bass and chord2melody AKA chord independent (bass and melody, separately)"
 python train_model.py --bass_or_melody=bass --piece_or_theme=piece
